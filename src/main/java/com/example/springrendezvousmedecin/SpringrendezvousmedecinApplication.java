7eeef85ece1daf4e3d1620c9e4d2ef420c475c37
lucaramiandrasoa <ramiandrasoaluca@gmail.com>
2023-11-04 12:45:49 +0100

    first commit

---
+++ src/main/java/com/example/springrendezvousmedecin/SpringrendezvousmedecinApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrendezvousmedecin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrendezvousmedecinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrendezvousmedecinApplication.class, args);
+	}
+
+}
